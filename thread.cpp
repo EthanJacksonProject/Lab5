91de26ff9fbdfc2bdddb09b8cf301c7549046f2c
Ethan Jackson <Ethanjackson12345@gmail.com>
2019-11-18 10:23:15 -0500

    Added Comments/Formatting

--- thread.cpp
+++ thread.cpp
@@ -8,110 +8,88 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
+//Path of data:
+// 1 - grab places it into an image_type struct
+// 2 - digitizer() puts that struct into a buffer
+// 3 - tracker() unpacks that buffer and retrieves a image_type
+// 4 - analyze() takes in that image_type and unpacks the img_data ptr to the img variable
+
 //Make image values global for ease of use
 int width, height, channels;
-int count = 0;
 
 typedef struct {
- unsigned char* img_data;
+    unsigned char* img_data;
 } image_type;
 
-image_type Grab(){ //Imports image and places it into memory
+image_type Grab(){ //Imports image and places it into memory, returns struct w/ ptr
 
-    //std::cout << count++ << std::endl;
     //STB_Image library: Read in an image as unsigned char
     unsigned char *img = stbi_load("inputFull.png", &width, &height, &channels, 0);
-    //STB_Image Error checking from example
-    if(img == NULL) {printf("Error in loading the image\n");exit(1);} //Error Checking, copied from stb_image library example
+    if(img == NULL) {printf("Error in loading the image\n");exit(1);} //STB_Image Error checking from example
     image_type image;
     image.img_data = img;
     return image;
 }
 
 void analyze(image_type image){ //Grabs image via pointer and performs transpose
-  
- // std::cout << count++ << std::endl;
-  unsigned char* img = image.img_data;
-  //Allocate space for new image
-     //Allocate space for new image
-  size_t img_size = width * height * channels;
-  unsigned char *new_img = (unsigned char*)malloc(img_size);
 
-     //STB_Image library error checking
-  if(new_img == NULL) {
-    printf("Unable to allocate memory for the gray image.\n");
-    exit(1);
-  }
-     //Allocate space for input to live as an array in memory for processing
-  //unsigned char out[img_size];
+  unsigned char* img = image.img_data; //Unpack img* from struct created in Grab()
+  size_t img_size = width * height * channels; 
+  unsigned char *new_img = (unsigned char*)malloc(img_size); //Allocate space for new image
+
+  if(new_img == NULL) {printf("Unable to allocate memory for the gray image.\n");exit(1);}//STB_Image library error checking
+  
+  //Initialize separate color layers for processing
   unsigned char red[width*height];
-unsigned char green[width*height];
-unsigned char blue[width*height];
-    
+  unsigned char green[width*height];
+  unsigned char blue[width*height];
 
+  //For Loop logic adapted from STB_Image example => splits img char[] into color channels for ease of processing
+  int k = 0;
+  for(unsigned char *p = img; p != img + img_size; p += channels) { 
+    red[k]   = (uint8_t)*(p);
+    green[k] = (uint8_t)*(p+1);
+    blue[k++] = (uint8_t)*(p+2); 
+  }
 
-  
-  //Taken from STB_Image library example => used to read image to array Out[] for processing
-int k = 0;
-  for(unsigned char *p = img; p != img + img_size; p += channels) { //It's segfaulting here
-   
-    //Path of the pointer to this loop:
-    // 1 - grab places it into an image_type struct
-    // 2 - digitizer() puts that struct into a buffer
-    // 3 - tracker() unpacks that buffer and retrieves a image_type
-    // 4 - analyze() takes in that image_type and unpacks the img_data ptr to the img variable
-    // 5 - this loop segfaults, but works without threading overhead and the digitize/tracker functions
+  //Perform transpose of each color layer, moves to colorT[]
+  int N = height;  
+  unsigned char blueT[width*height];
+  unsigned char greenT[width*height];
+  unsigned char redT[width*height];
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < N; j++){
+     blueT[j + (i * N)]   = blue[(j * N) + i];
+     greenT[ j + (i * N)] = green[(j * N) + i];
+     redT[j + (i * N)] = red[(j * N) + i];
+    }
+  }
 
-  //std::cout << *(p) << std::endl;
- red[k]   = (uint8_t)*(p);
-  green[k] = (uint8_t)*(p+1);
-  blue[k++] = (uint8_t)*(p+2); 
-}
-
-
-     //Divide input string to each color layer to be processed individually
-
-      //Optimize this into other loop
-   
-     //Perform transpose of each color layer => rotates image CW 90 degrees
-        int N = height;  
-
-        unsigned char blueT[width*height];
-        unsigned char greenT[width*height];
-        unsigned char redT[width*height];
-        for(int i = 0; i < N; i++){
-          for(int j = 0; j < N; j++){
-           blueT[j + (i * N)] = blue[(j * N) + i];
-           greenT[ j + (i * N)] = green[(j * N) + i];
-           redT[j + ( i * N)] = red[(j * N) + i];
-         }
-       }
-
-
-   unsigned char redTR[width*height];
-   unsigned char blueTR[width*height];
-   unsigned char greenTR[width*height];
-   k = 0;
-   for(int row = 0; row < height; ++row){
+  //Flips each row of image => combines with tranpose operation to rotate image 90 degrees => written to colorRT
+  unsigned char redTR[width*height];
+  unsigned char blueTR[width*height];
+  unsigned char greenTR[width*height];
+  k = 0;
+  for(int row = 0; row < height; ++row){
     for(int index = width-1; index >= 0; --index){
       redTR[width*row + index] = redT[k];
       blueTR[width*row + index] = blueT[k];
       greenTR[width*row + index] = greenT[k++];
     }
-   }
-
-     //Rebuilds processed output image from each color layer
-   unsigned char Recon[width*height*channels];
-   for(int i = 0; i < width * height; ++i){
-    Recon[3 * i+0] = redT[i];
-    Recon[3 * i+1] = greenT[i];
-    Recon[3 * i+2] = blueT[i];
   }
 
-     //STB_Image: Create output image and free memory and allocated space
+  //Rebuilds processed output image from each color layer
+  unsigned char Recon[width*height*channels];
+  for(int i = 0; i < width * height; ++i){
+   Recon[3 * i + 0] = redT[i];
+   Recon[3 * i + 1] = greenT[i];
+   Recon[3 * i + 2] = blueT[i];
+  }
+
+  //STB_Image: Create output image and free memory and allocated space
   stbi_write_png("Output.png", width, height, channels, Recon, width * channels);
   stbi_image_free(img);
-  free(new_img);
+  free(new_img); //Free malloced space for output image after writing
   return;
 }
 
@@ -136,21 +114,21 @@ void* digitizer(void* a) { //handles mutex locking and calls grab() for processi
   
   while(true){
     dig_image = Grab(); //Grab() Takes care of repeatedlty grabbing the image into memory
-    pthread_mutex_lock(&buflock);
+    pthread_mutex_lock(&buflock); //Grab buffer, then wait for opening
     
-    if (bufavail == 0){
-      pthread_cond_wait(&buf_notempty,&buflock);     
+    if (bufavail == 0){ //No space => wait for opening
+      pthread_cond_wait(&buf_notempty,&buflock); //Proceeds when buffer space opens     
     }
     pthread_mutex_unlock(&buflock);
 
-    frame_buf[tail % MAX] = dig_image;
-    tail = tail + 1; 
-    pthread_mutex_lock(&buflock);
-    bufavail = bufavail - 1;
+    frame_buf[tail % MAX] = dig_image; //Load image_type opbject into buffer
+    tail = tail + 1; //Move tail to last object
+    pthread_mutex_lock(&buflock); //Lock to udpate buffer info
+    bufavail = bufavail - 1; //Reduce space by 1 since an object has been placed in
 
-    pthread_mutex_unlock(&buflock);
+    pthread_mutex_unlock(&buflock); //Open lock
     if(bufavail < MAX){
-      pthread_cond_broadcast(&buf_notfull);
+      pthread_cond_broadcast(&buf_notfull); //Broadcast that there is space to tracker()
     }
   }
   return 0; 
@@ -162,23 +140,23 @@ void* tracker(void* a) { //handles mutex locking and calls anaylyze() for proces
   
   while(true){
 
-    pthread_mutex_lock(&buflock);
+    pthread_mutex_lock(&buflock); 
     if (bufavail == MAX){
-      pthread_cond_wait(&buf_notfull, &buflock);
+      pthread_cond_wait(&buf_notfull, &buflock); //wait for at least one object to be entered
     }
     pthread_mutex_unlock(&buflock);
 
-    track_image = frame_buf[head % MAX];
-    head = head + 1;
+    track_image = frame_buf[head % MAX]; //Load image to local memory
+    head = head + 1; //update buffer location stats
     pthread_mutex_lock(&buflock);
-    bufavail = bufavail + 1;
+    bufavail = bufavail + 1; //Add space since an object is being processed
     pthread_mutex_unlock(&buflock);
     if(bufavail > 0){
-      pthread_cond_broadcast(&buf_notempty);
+      pthread_cond_broadcast(&buf_notempty); //Broadcast that space is available for Digitize()
     }
-    analyze(track_image);
+    analyze(track_image); //Pass image_type object to analyze() for processing
   }
-  return 0;
+  return;
 } 
 
 
@@ -188,7 +166,10 @@ int main() {
     /* Create independent threads each of which will execute function */
  
  pthread_create(&Digitize, NULL, digitizer, NULL);
+ int pthread_setname_np(Digitize, "Digitizer"); //Stat Tracking
+ 
  pthread_create(&Tracker, NULL, tracker, NULL);
+ int pthread_setname_np(Tracker, "Tracker"); //Stat Tracking
 
      /* Wait till threads are complete before main continues. Unless we  */
      /* wait we run the risk of executing an exit which will terminate   */
